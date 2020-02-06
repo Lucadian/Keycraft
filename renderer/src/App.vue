3194fa891749674addb606d1fbd30b87619c3000
Barry Liu <742848233@qq.com>
2020-02-07 06:58:30 +0800

    .

---
+++ renderer/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <aside>
+      <h5 id="title">快捷键设置</h5>
+      <ul id="camp">
+        <li>人类联盟</li>
+        <li>暗夜精灵</li>
+        <li>兽人部落</li>
+        <li>不死亡灵</li>
+        <li>中立</li>
+      </ul>
+      <button id="option">选项</button>
+      <button id="save">保存</button>
+    </aside>
+    <main>
+      <div id="units"></div>
+      <div id="archs"></div>
+      <div id="buttons"></div>
+      <div id="input"></div>
+      <div id="infos"></div>
+    </main>
+  </div>
+</template>
+
+<style lang="scss">
+
+</style>
